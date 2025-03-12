6fc02785b0c15d709ff7c0f84029c372d9132f66
FixedAmateur <12a1tranphamtuan@gmail.com>
2025-03-12 13:58:29 +0700

    initial commit

---
+++ pet-supermarket-backend/pet-supermarket/src/main/java/com/laborexport/pet_supermarket/PetSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.laborexport.pet_supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSupermarketApplication.class, args);
+	}
+
+}
---
+++ pet-supermarket-backend/pet-supermarket/src/test/java/com/laborexport/pet_supermarket/PetSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laborexport.pet_supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
